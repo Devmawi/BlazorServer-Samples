153f720fd836eeb9e35c82c0eb3d9c5821c7bad3
dev <dev@dev.org>
2021-11-21 15:08:15 +0100

    add configuration example

---
+++ ConfigurationManagement/AppOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationManagement
+{
+    public class AppOptions
+    {
+        public string ConfigurationValue { get; set; }
+    }
+}
